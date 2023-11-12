ac8fa5b3507b03e9ffc4b4990bd50c66ce8e9b5b
primostasis123 <editomoncada123@gmail.com>
2023-11-13 01:55:30 +0800

    add prisma adapter
    add prisma adapter, use google, github as provider in login

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,23 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/GitHub"
-import Google from "next-auth/providers/Google"
-export const { handlers, auth } = NextAuth({ providers: [ GitHub ] })
+import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export const {handlers, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
+    }),
+    GoogleProvider({
+        clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+        allowDangerousEmailAccountLinking: true,
+      }),
+  ],
+})
